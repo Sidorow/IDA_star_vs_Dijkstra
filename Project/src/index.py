ee455536b78f5c3c957b3ae6fbf58379e6d52c2d
Sidoroff Sami L S <samsidor@cubbli22-gold.local>
2023-04-21 21:27:51 +0300

    console user interface

--- Project/src/index.py
+++ Project/src/index.py
@@ -1,27 +1,8 @@
-from Dijkstra import Dijkstra
-from IDA_star import IDA_star
-from graphGen import graphGen
+from UI.ui import UI
 
 def main():
-    graphgen_obj = graphGen(10,14) # Graph generator object
-    fixed_graph = graphgen_obj.get_fixed_graph() # Fixed graph for easy illustration purposes
-    random_graph = graphgen_obj.gen_random_graph()
-
-    start = 'A'
-    goal = 'G'
-
-    dijkstra = Dijkstra(random_graph[0], start, goal)
-    ida_star = IDA_star(random_graph[0], random_graph[1], start, goal)
-
-    distances = dijkstra.get_distances()
-    path_dijkstra = dijkstra.get_path()
-    path_ida = ida_star.get_path()
-
-    print(f"Distances from {start}: {distances} \n")
-    print(f"Dijkstra path = {path_dijkstra} \n")
-    print(f"IDA* path = {path_ida} \n")
-    graphgen_obj.gen_graph_plot(random_graph[0], random_graph[1], path_ida)
-
+    ui = UI()
+    ui.initialize()
 
 if __name__ == "__main__":
     main()
